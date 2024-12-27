5f8f5d8619d85a632d95b4ffcdc039b1d49a64a8
Jordan Kowal <kowaljordan@gmail.com>
2024-12-27 15:38:56 +0100

    chore: removed no-longer used utils

--- pipe_operator/shared/utils.py
+++ pipe_operator/shared/utils.py
@@ -16,15 +16,3 @@ def is_one_arg_lambda(f: Callable) -> bool:
     """Check if a function is a lambda with exactly and only 1 positional parameter."""
     sig = inspect.signature(f)
     return is_lambda(f) and len(sig.parameters) == 1
-
-
-def function_needs_parameters(f: Callable) -> bool:
-    """Checks if a function has at least one positional/keyword parameter."""
-    sig = inspect.signature(f)
-    for _name, param in sig.parameters.items():
-        if param.default == inspect.Parameter.empty and param.kind in (
-            inspect.Parameter.POSITIONAL_ONLY,
-            inspect.Parameter.POSITIONAL_OR_KEYWORD,
-        ):
-            return True
-    return False
